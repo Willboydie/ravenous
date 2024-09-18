f5d1c498d6baf0ab57d88166493aafdf3e64fa6d
= <=>
2024-09-18 09:50:55 +0100

    Made searchbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import sushi from './happy-man-eating-sushi.webp'
+import searchBar from './SeachBar';
 
 function App() {
   return (
     <div className="App" >
       <header className="App-header">
-        <h1 type="Big">Ravenous</h1>
-        <img src={sushi} alt="sushiman" height='300' width='250'/>
+        <h1 type='Big'>Ravenous</h1>
+        {searchBar}
+        <img src={sushi} alt="sushiman" height='300' width='250'
+        style={{ padding: '20px'}}/>
         <p>
           Feed yourself. Feed your dreams. 
         </p>
         <span>
-        <a>An app by </a>
-        <a
+        <a An app by 
           className="App-link"
           href="https://willboydtutoring.co.uk"
           target="_blank"
---
+++ src/Components/BusinessList.js
@@ -0,0 +1,17 @@
+import Business from './Business';
+
+const tonysSushi = new Business(
+    './happy-man-eating-sushi.webp',
+    'Tony\'s Sushi',
+    '120 George Street',
+    'Edinburgh',
+    'Scotland',
+    'EH1 3PX',
+    9.1,
+    17
+);
+
+const BusinessList = [tonysSushi];
+
+
+export default BusinessList;
