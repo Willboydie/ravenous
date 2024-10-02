8f4a60ad7c631d4d4be755ecb1b772cfd431d2f2
= <=>
2024-10-02 15:05:51 +0100

    add fetch capability for older browsers

---
+++ src/utils/yelpApi.js
@@ -0,0 +1,4 @@
+
+const clientId = '3psywHDO_6cr39Vaueaolw';
+const yelpApiKey = '_UhWFub-mYNzfnnw2i2Zqc0ramMQtYLp4wA6vvgentsG-oVWPZvC1jFX0qdRNd6_YKNk7DqVLIDgV8YdkivrTvOkk_A6lGrW_oJZpAHbGc20qiKLu7DS_700j0_9ZnYx';
+
